e31855eb483dadc4fa0f13b50e432b02956992ef
Alex <alexrtucker@hotmail.co.uk>
2019-12-03 16:29:56 +0000

    Exercise 4 complete

--- exercise_4.rb
+++ exercise_4.rb
@@ -6,6 +6,39 @@
 # other formats
 
 def pig_latin(word)
+capital = false
+  if word.capitalize == word
+     capital = true
+  end
+
+  if "aeiou".include? word.chr
+
+    word += "way"
+
+  else
+
+    if word[0..1] == "qu"
+
+      word += word[0..1] + "ay"
+      word = word[2..-1]
+
+    else
+
+      until "aeiou".include? word.chr
+        word += word.chr
+        word = word[1..-1]
+      end
+      word += "ay"
+
+    end
+
+  end
+
+  if capital == true
+    word = word.capitalize
+  end
+
+word
 
 end
 
